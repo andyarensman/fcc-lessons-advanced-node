b485c3a22940bb1628a747ab286f4f6435804227
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-07-22 10:45:46 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,124 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
+const session = require('express-session');
+const passport = require('passport');
+
+const ObjectID = require('mongodb').ObjectID;
+
+
+const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+//part 20 requires
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  //not sure if next two lines are right
+  key: 'express.sid',
+  store: store,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+  
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
+
+  //not sure if these two functions go here
+  function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+  //is this right? I think so
+  let currentUsers = 0;
+
+  io.on('connection', socket => {
+    ++currentUsers;
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
+
+    console.log('user ' + socket.request.user.name + ' connected');
+    //console.log('A user has connected');
+
+    socket.on('disconnect', () => {
+      /*anything you want to do on disconnect*/
+      --currentUsers;
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
+      console.log('A user has disconnected');
+    });
+
+    socket.on('chat message', message => {
+      io.emit('chat message', {name: socket.request.user.name, message})
+    })
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
+
+app.set('view engine', 'pug')
+//app.set('views', './views/pug')
+
+
+
